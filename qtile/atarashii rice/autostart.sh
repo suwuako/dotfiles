2b87268a6048926459d2390690ec6e806e9154a0
megumin00 <43295199+megumin00@users.noreply.github.com>
2021-03-03 15:03:14 +0000

    Update autostart.sh

--- qtile/atarashii rice/autostart.sh
+++ qtile/atarashii rice/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-feh --bg-scale /usr/share/backgrounds/Atarashii.jpg &
+feh --bg-scale ~/.config/qtile/Atarashii.jpg &
 vim ~/.config/qtile/config.py &
 compton &
 xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output USB-C-0 --off --output DP-1-1 --off --output HDMI-1-1 --mode 1920x1080 --pos 2560x360 --rotate normal &
